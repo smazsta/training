cb4b510c6fe12a169924014007e75d43a148d2c0
Stala Mateusz-WCGK37 <mateusz.stala@motorolasolutions.com>
2018-11-14 11:25:36 +0100

    CodeWars - Scramblies refactored

--- codeWars/src/main/java/scramblies/Scramblies.java
+++ codeWars/src/main/java/scramblies/Scramblies.java
@@ -7,23 +7,6 @@ import java.util.stream.Stream;
 
 public class Scramblies {
 
-    public static boolean scramble2(String str1, String str2) {
-        List<String> array2 = Stream.of(str2.split("")).collect(Collectors.toList());
-        List<String> array1 = Stream.of(str1.split("")).collect(Collectors.toList());
-        List<String> result = new ArrayList<>();
-        for (int i = 0; i < array2.size(); i++) {
-            if (array1.contains(array2.get(i))) {
-                result.add(array2.get(i));
-                array1.remove(array2.get(i));
-            }
-        }
-        if(result.size() == str2.length()){
-            return true;
-        }
-
-        return false;
-    }
-
     public static boolean scramble(String str1, String str2) {
         if (str2.length() > str1.length()) return false;
         for (String s: str2.split("")) {
